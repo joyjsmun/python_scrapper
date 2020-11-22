4399b2a4f7dc4fd76cc2945aae407630e6393b9b
Joy Js Mun <joy.js.mun@gmail.com>
2020-11-22 12:43:47 +0000

    N_add csv function, M_modified main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from indeed import get_jobs as get_indeed_jobs
 from st import get_jobs as get_st_jobs
+from save import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 st_jobs = get_st_jobs()
 
 jobs = st_jobs + indeed_jobs
+save_to_file(jobs)
+
 
 
 
